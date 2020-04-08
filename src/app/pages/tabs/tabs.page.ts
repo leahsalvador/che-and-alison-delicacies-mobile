e40e61b697d787b7b4e762f4b5194c3ad2069a75
leahsalvador <52876919+leahsalvador@users.noreply.github.com>
2020-04-09 02:00:04 +0800

    Update capacitor from 1.5.2 to 2.0.0

--- src/app/pages/tabs/tabs.page.ts
+++ src/app/pages/tabs/tabs.page.ts
@@ -1,4 +1,16 @@
 import { Component } from '@angular/core';
+//API
+import {
+  Plugins,
+  StatusBarStyle,
+} from '@capacitor/core';
+
+// Events (iOS only)
+window.addEventListener('statusTap', function () {
+  console.log("statusbar tapped");
+});
+
+const { StatusBar } = Plugins;
 
 @Component({
   selector: 'app-tabs',
@@ -7,6 +19,30 @@ import { Component } from '@angular/core';
 })
 export class TabsPage {
 
-  constructor() {}
+  constructor() {
+  }
+  isStatusBarLight = true
 
+  changeStatusBar() {
+    Plugins.StatusBar.setBackgroundColor({
+      color: '#172a65'
+    });
+    Plugins.StatusBar.setStyle({
+      style: this.isStatusBarLight ? StatusBarStyle.Dark : StatusBarStyle.Light
+    });
+    this.isStatusBarLight = !this.isStatusBarLight;
+
+    // Display content under transparent status bar (Android only)
+    /*Plugins.StatusBar.setOverlaysWebView({
+      overlay: true
+    });*/
+  }
+
+  hideStatusBar() {
+    Plugins.StatusBar.hide();
+  }
+
+  showStatusBar() {
+    Plugins.StatusBar.show();
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,26 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+//API
+import {
+  Plugins,
+  StatusBarStyle,
+} from '@capacitor/core';
+
+// Events (iOS only)
+window.addEventListener('statusTap', function () {
+  console.log("statusbar tapped");
+});
+
+let isStatusBarLight = true;
+
+Plugins.StatusBar.setBackgroundColor({
+  color: '#172a65'
+});
+Plugins.StatusBar.setStyle({
+  style: isStatusBarLight ? StatusBarStyle.Dark : StatusBarStyle.Light
+});
+isStatusBarLight = !isStatusBarLight;
 
 if (environment.production) {
   enableProdMode();
